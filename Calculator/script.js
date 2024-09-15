2f491c0d81b7d78ec53ec1ceda2cebac3d487a3e
Sakshi-260204 <149154989+Sakshi-260204@users.noreply.github.com>
2024-09-15 19:56:19 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,62 @@
+let currentOperand = '';
+let previousOperand = '';
+let operation = undefined;
+
+function appendNumber(number) {
+    if (number === '.' && currentOperand.includes('.')) return;
+    currentOperand += number;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('display').innerText = currentOperand;
+}
+
+function chooseOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        compute();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+}
+
+function compute() {
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        case '*':
+            computation = prev * current;
+            break;
+        case '/':
+            computation = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = computation.toString();
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentOperand = '';
+    previousOperand = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function deleteNumber() {
+    currentOperand = currentOperand.slice(0, -1);
+    updateDisplay();
+}
